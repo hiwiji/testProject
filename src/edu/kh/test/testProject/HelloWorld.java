9c647493cf7f9145ff7fc976774da685f50eb4ef
jieun <jjewwi12@gmail.com>
2022-12-06 10:47:04 +0900

    Second commit

--- src/edu/kh/test/testProject/HelloWorld.java
+++ src/edu/kh/test/testProject/HelloWorld.java
@@ -3,6 +3,8 @@ package edu.kh.test.testProject;
 public class HelloWorld {
     
 	public static void main(String[] args) {
+		
+		
 		// 실행구문 (메인메소드가 있어야 프로젝트 실행 가능)
 		System.out.println("Hello World~!"); //영문
 		System.out.println("아침은 드셨나요?");  //한글
@@ -17,14 +19,17 @@ public class HelloWorld {
 	    // " " 안에 작성되지 않은 코드는 숫자, 변수로 인식
 		
 		// " " (문자열) + 숫자 함께 작성 
-		System.out.println("14 * 19 =" +266);
+		System.out.println("14 * 19 =" +266);   // 14*19 = 266
 		// 숫자와 숫자끼리의 덧셈은 
 		// 실제 덧셈 연산결과.
 		// 문자열과 숫자 or 문자열과 문자열의 덧셈은 
 		// ==> 이어쓰기
-		System.out.println("14 * 19 = " + 14 *19); //
+		System.out.println("14 * 19 = " + 14 *19); // 14*19 = 266
 		
+		// notice!
+		// 자바는 사칙연산의 우선순위를 그대로 따른다.
+		// ex) 우선 계산을 원하면 () 괄호 사용
 		
-	
+	    System.out.println("안녕? 날 보고있니?"); //깃허테스트용
 	}
 }
